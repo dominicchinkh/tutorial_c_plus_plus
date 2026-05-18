188ce4ea3fc892dfc38c8c5d5f7196eea3bca367
Dominic Chin <dominicchinkh@gmail.com>
2026-05-19 06:34:22 +1000

    Add another book (and move existing files to its book)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vector>
-// #include "src/chapter1/IntCell.h"
-// #include "src/chapter1/Recursion.h"
-// #include "src/chapter1/ClassTemplate.h"
-// #include "src/chapter1/FunctionObject.h"
-// #include "src/chapter1/FunctionTemplate.h"
-#include "src/chapter1/Matrix.h";
-// #include "src/chapter1/Square.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter1/IntCell.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter1/Recursion.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter1/ClassTemplate.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter1/FunctionObject.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter1/FunctionTemplate.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter1/Matrix.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter1/Square.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter2/BinarySearch.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter2/Exponentiation.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter2/GreatestCommonDivisor.h"
+// #include "src/DataStructuresAndAlgorithmAnalysis/chapter2/MaximumSubsequence.h"
 
 using namespace std;
 
