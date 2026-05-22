346f18be1f2bde51e89e7f09644b2f0f1e775b08
Dominic Chin <dominicchinkh@gmail.com>
2026-05-23 07:32:25 +1000

    Add stack on vector

--- src/book/data-structures-and-algorithm-analysis/chapter3/vector-stack.h
+++ src/book/data-structures-and-algorithm-analysis/chapter3/vector-stack.h
@@ -4,6 +4,28 @@
 #include <stdexcept>
 #include <vector>
 
+/*
+ *  int main(int argc, char *argv[]) 
+ *  {
+ *      VectorStack<int> a;
+ *  
+ *      cout << "Current size: " << a.size() << endl;
+ *      cout << "Is empty? " << (a.empty() ? 'Y':'N') << endl;
+ *     
+ *      a.push(47);
+ *      a.push(19);
+ *      a.push(73);
+ *  
+ *      cout << "`a` new size: " << a.size() << endl;
+ *      cout << "Is empty? " << (a.empty() ? 'Y':'N') << endl;
+ *      
+ *      for (; !a.empty();) {
+ *          cout << "`a` element: " << a.pop() << endl;
+ *      }
+ *  
+ *      return 0;
+ *  }
+ */
 template <typename Object>
 class VectorStack
 {
